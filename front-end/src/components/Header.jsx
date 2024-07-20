b8624e616c792933f5bc2e8978ce75026127dbdb
Luz Lamas <144249539+llamaz01@users.noreply.github.com>
2024-07-20 17:15:50 -0400

    Update Header.jsx

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,6 +1,6 @@
-'use client'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
 
-import { useState } from 'react'
 import {
   Dialog,
   DialogPanel,
@@ -11,7 +11,7 @@ import {
   PopoverButton,
   PopoverGroup,
   PopoverPanel,
-} from '@headlessui/react'
+} from '@headlessui/react';
 import {
   ArrowPathIcon,
   Bars3Icon,
@@ -20,8 +20,8 @@ import {
   FingerPrintIcon,
   SquaresPlusIcon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+} from '@heroicons/react/24/outline';
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
 
 const products = [
   { name: 'Hoteles', description: 'Encuentra los mejores hoteles para ti', href: '#', icon: ChartPieIcon },
@@ -29,14 +29,24 @@ const products = [
   { name: 'Playas', description: 'Entretenimiento y el Calor del verano', href: '#', icon: FingerPrintIcon },
   { name: 'Plazas', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
   { name: 'Monumentos', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
+];
+
 const callsToAction = [
   { name: 'Conoce Nuestra ciudad', href: '#', icon: PlayCircleIcon },
   { name: 'Contactanos', href: '#', icon: PhoneIcon },
-]
+];
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const navigate = useNavigate(); // Usa useNavigate aquí
+
+  const handleOutLogin = () => {
+    // Elimina el token de autenticación (ajusta esto según tu implementación)
+    localStorage.removeItem('token'); // O sessionStorage, según cómo almacenes el token
+    
+    // Redirige al usuario a la página de inicio
+    navigate('/'); // Redirige a la página de inicio
+  };
 
   return (
     <header className="bg-white">
@@ -110,7 +120,7 @@ export default function Header() {
           </a>
         </PopoverGroup>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900" onClick={handleOutLogin}>
             Log Out <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
@@ -189,5 +199,5 @@ export default function Header() {
         </DialogPanel>
       </Dialog>
     </header>
-  )
+  );
 }
