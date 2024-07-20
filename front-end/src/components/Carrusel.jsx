86bc7669b34ba4863e9af9ee744a9807bee3bc98
Luz Lamas <144249539+llamaz01@users.noreply.github.com>
2024-07-20 16:49:12 -0400

    Update Carrusel.jsx

--- front-end/src/components/Carrusel.jsx
+++ front-end/src/components/Carrusel.jsx
@@ -1,73 +1,29 @@
-import { Carousel, IconButton } from "@material-tailwind/react";
- 
-export default function Carrusel() {
-  return (
-    <Carousel
-      className="rounded-xl"
-      prevArrow={({ handlePrev }) => (
-        <IconButton
-          variant="text"
-          color="white"
-          size="lg"
-          onClick={handlePrev}
-          className="!absolute top-2/4 left-4 -translate-y-2/4"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-            />
-          </svg>
-        </IconButton>
-      )}
-      nextArrow={({ handleNext }) => (
-        <IconButton
-          variant="text"
-          color="white"
-          size="lg"
-          onClick={handleNext}
-          className="!absolute top-2/4 !right-4 -translate-y-2/4"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-            />
-          </svg>
-        </IconButton>
-      )}
-    >
-      <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-        alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="image 2"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="image 3"
-        className="h-full w-full object-cover"
-      />
-    </Carousel>
-  );
+import { Carousel } from "@material-tailwind/react";
+
+function Carrusel() {
+    return (
+        <div className="flex justify-center py-8">
+            <div className="w-400 h-400">
+                <Carousel transition={{ duration: 2 }} className="rounded-xl h-full w-full">
+                    <img
+                        src="https://b01aa32c.rocketcdn.me/wp-content/uploads/2022/12/SanRoqueEncarnacion.jpg"
+                        alt="image 1"
+                        className="h-full w-full object-cover rounded-xl"
+                    />
+                    <img
+                        src="https://masencarnacion.s3.us-west-2.amazonaws.com/uploads/public/635/80f/41a/63580f41a51ad859699708.png"
+                        alt="image 2"
+                        className="h-full w-full object-cover rounded-xl"
+                    />
+                    <img
+                        src="https://grupovierci.brightspotcdn.com/dims4/default/1f5405e/2147483647/strip/true/crop/720x405+0+37/resize/1000x563!/quality/90/?url=https%3A%2F%2Fk2-prod-grupo-vierci.s3.us-east-1.amazonaws.com%2Fbrightspot%2Fe9%2F39%2Fe96ecb6f4eceadf6234ac56f6785%2Fencarnacion-whatsapp-image-2024-01-01-at-11-24-37-49052298.jpeg"
+                        alt="image 3"
+                        className="h-full w-full object-cover rounded-xl"
+                    />
+                </Carousel>
+            </div>
+        </div>
+    );
 }
+
+export default Carrusel;
